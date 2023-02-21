8d64a2b0393512315c3be1350bcfadb490fbbbc9
Simen Abelsen <simen@abelweb.no>
2023-02-22 00:08:40 +0100

    :alembic: Add JotaiPeople experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Card, Counter } from "@/components";
+import { Card, Counter } from '@/components';
+import { JotaiPeople } from './components/JotaiPeople';
 
 function App() {
   return (
@@ -6,17 +7,18 @@ function App() {
       <header className="container mx-auto bg-white rounded-md flex p-4">
         <h1 className="text-2xl font-semibold">Jotai React Playground</h1>
       </header>
-      <main className="container mx-auto flex-1">
+      <main className="container mx-auto flex-1 space-y-4">
         <div className="flex gap-4 flex-wrap">
           <Card className="flex-1">
             <h3 className="text-xl semi-bold">Hello React + Vite!</h3>
             <h4 className="text-lg">
-              This is a starter application for a React + Vite app with Vitest,
-              Prettier and TailwindCSS setup.
+              This is a starter application for a React + Vite app with Vitest, Prettier and
+              TailwindCSS setup.
             </h4>
           </Card>
           <Counter />
         </div>
+        <JotaiPeople />
       </main>
     </div>
   );
