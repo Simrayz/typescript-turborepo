40c1cc02b9235d4126fba2ad7238969cde513abf
Simen Abelsen <simen@abelweb.no>
2023-02-28 17:36:50 +0100

    :recycle: Setup monorepo with jotai-react

--- apps/jotai-react/src/utils/classNames.ts
+++ apps/jotai-react/src/utils/classNames.ts
@@ -1,12 +1,12 @@
 export function classNames(...classes: (string | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  const testClasses = ["test", undefined, "completed"];
+  const testClasses = ['test', undefined, 'completed'];
 
-  it("add", () => {
-    expect(classNames(...testClasses)).toBe("test completed");
+  it('add', () => {
+    expect(classNames(...testClasses)).toBe('test completed');
   });
 }
